7b56059cbbdf6c1bec17e3ea9bb244807a179f23
Pawat Rattanasom <pawatrattanasom@Pawats-Air.home>
2024-03-19 11:35:24 +0700

    update

--- src/components/PromoteCard.tsx
+++ src/components/PromoteCard.tsx
@@ -2,22 +2,27 @@
 import { Plaster } from "next/font/google";
 import  VideoPlayer from "./VideoPlayer";
 import { useState } from "react";
+import { useWindowListener } from "@/hooks/useWindowListener";
 
 export default function PromoteCard() {
 
     const [playing , setPlaying] = useState(true);
+
+    useWindowListener("contextmenu", (e)=> {
+        e.preventDefault();
+    });
+
     return (
         <div className="w-[80%] shadow-md mx-[10%] my-10 p-2 m-3 rounded-lg bg-gray-100 flex gap-3 flex-row">
             <VideoPlayer vdoSrc="/video/getvaccine.mp4" isPlaying={playing}></VideoPlayer>
-            <div>
-                Get Your Vaccine Today
-                <button className="block bg-cyan-500 text-white border border-transparent font-semibold py-2 px-2 m-2
-                    hover:bg-white hover:text-cyan-600 border-cyan-600 rounded-md shadow-lg shadow-blue-500/50" 
+            <div className="flex flex-col">
+                <h3>Get Your Vaccine Today </h3>
+                <div className="flex-grow"></div>
+                <button className="block bg-cyan-500 text-white border border-transparent font-semibold py-2 px-2 m-3
+                    hover:bg-white hover:text-cyan-600 border-cyan-600 rounded-md shadow-lg shadow-blue-500/50 w-[100px]" 
                     onClick={()=>(setPlaying(!playing)) }>
                         { playing ? "Pause" : "Play"}
                 </button>
-
-
             </div>
         </div>
     )
--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useRef , useEffect} from "react";
+import { useWindowListener } from "@/hooks/useWindowListener";
 
 
 export default function VideoPlayer( {vdoSrc, isPlaying} : {vdoSrc: string, isPlaying: boolean} ) {
@@ -11,7 +12,9 @@ export default function VideoPlayer( {vdoSrc, isPlaying} : {vdoSrc: string, isPl
         } else {
             vdoRef.current?.pause();
         }
-    });
+    }, [isPlaying]);
+
+    useWindowListener("resize", (e)=>{alert("Window Resized" + (e.target as Window).innerWidth) });
   return (
     <video src={vdoSrc} className="w-[40%]"  ref={vdoRef} controls loop muted />
   );
